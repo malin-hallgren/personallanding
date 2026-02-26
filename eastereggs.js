03f0b08b356b466baa6ceca2fca20b4cd27f589e
Malin Hallgren <malin.m.hallgren@gmail.com>
2026-02-26 09:55:02 +0100

    script cleanup

--- eastereggs.js
+++ eastereggs.js
@@ -1,3 +1,10 @@
+const secret = ["h", "e", "l", "l", "o"];
+let input = [];
+
+const easterModal = document.getElementById("easter-modal");
+const closeButton = document.getElementById("easter-close");
+
+/* Theme Switcher */
 function getCurrentTheme() {
     return localStorage.getItem('theme') === 'dark' ? 'dark' : 'light';
 }
@@ -30,4 +37,36 @@ function toggleTheme() {
 document.addEventListener('DOMContentLoaded', function() {
     applyTheme(getCurrentTheme());
     document.getElementById('theme-button')?.addEventListener('click', toggleTheme);
+});
+
+/* Easter Egg Modal */
+
+document.addEventListener("keydown", (keyPressed) => {
+    if (document.activeElement.tagName === "INPUT" || document.activeElement.tagName === "TEXTAREA") {
+        return; // Ignore key presses when typing in input fields or textareas
+    }
+
+    input.push(keyPressed.key.toLowerCase());
+
+    if (input.length > secret.length) {
+        input.shift();
+    }
+
+    if (input.join("") === secret.join("")) {
+        showEasterModal();
+        input = [];
+    }
+});
+
+function showEasterModal() {
+    easterModal.classList.add("show");
+}
+
+function hideEasterModal() {
+    easterModal.classList.remove("show");
+}
+
+closeButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    hideEasterModal();
 });
